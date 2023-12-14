27c7e4c6f4b076d05714710f1e5726e126d9037c
Alexander Söderwall <alexander.soderwall@gmail.com>
2023-12-14 10:24:25 +0100

    Added comments to main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,15 @@
-//Finding the various elements and giving them event listeners
+//Finding the various text-based elements and giving them event listeners
 const pageTitel = document.getElementById("pageTitle");
 pageTitel.addEventListener("input", inputPageTitle);
 const cardTextArea = document.getElementById("cardTextArea");
 cardTextArea.addEventListener("input", inputTextArea);
-
+//Finds body and the relevant text field. Gives the text field an event listener
 const body = document.getElementById("bodyBackground");
 const backgroundTextInput = document.getElementById("backgroundTextInput");
 const backgroundSubmitButton = document.getElementById(
   "backgroundSubmitButton"
 );
 backgroundSubmitButton.addEventListener("click", getBackground);
-// body.style.backgroundImage =
-//   "url('https://img.etimg.com/thumb/width-640,height-480,imgsize-56196,resizemode-75,msid-95423731/magazines/panache/5-reasons-why-tomatoes-should-be-your-favourite-fruit-this-year/tomatoes-canva.jpg')";
 
 //Functions for the text fields
 function inputPageTitle(inputText) {
@@ -24,36 +22,37 @@ function inputTextArea(textArea) {
   cardTextArea.value = localStorage.getItem("textArea");
 }
 
+//Function to get a background
 function getBackground() {
+  //First checks if the field is empty or not
   if (backgroundTextInput.value.length === 0) {
     console.log("String is empty");
   } else {
+    //Takes the value from the input field and puts it into the API's given parameters
     let api_url = `https://api.unsplash.com/photos/random?query=${backgroundTextInput.value}&client_id=ScnUMlifBs2ManUdhxd35ZzlYT4o3EQvbt-c-4277sY`;
+    //Sends along the "convereted" text field input to the API function
     getBackgroundAPI(api_url);
   }
-  // let newBackgroundImage = `https://api.unsplash.com/photos/random?query=${backgroundTextInput.value}&client_id=ScnUMlifBs2ManUdhxd35ZzlYT4o3EQvbt-c-4277sY`;
-  // console.log(newBackgroundImage);
-  console.log("hello");
 }
+
 async function getBackgroundAPI(api_url) {
   const response = await fetch(api_url);
   if (response.ok) {
     const apiImageData = await response.json();
-    console.log(apiImageData.urls.full);
+    //Puts the image URL from the API into local storage
     localStorage.setItem("backgroundImage", `${apiImageData.urls.regular}`);
+    //Applys the new background image to the body
     body.style.backgroundImage = `url('${localStorage.getItem(
       "backgroundImage"
     )}')`;
-    // body.style.backgroundImage = `url('${apiImageData.urls.full}')`;
   } else {
-    console.log("jsonError");
+    console.log("JSON Error");
   }
 }
 
-if (localStorage.getItem("backgroundImage") != null) {
-  body.style.backgroundImage = `url('${localStorage.getItem(
-    "backgroundImage"
-  )}')`;
-}
-
-//https://api.unsplash.com/photos/random?query=${term}&client_id=ScnUMlifBs2ManUdhxd35ZzlYT4o3EQvbt-c-4277sY
+//Upon loading the site it will check if a background has already been applied by the user via local storage
+localStorage.getItem("backgroundImage") != null
+  ? (body.style.backgroundImage = `url('${localStorage.getItem(
+      "backgroundImage"
+    )}')`)
+  : console.log("LocalStorage backrgound img error");
